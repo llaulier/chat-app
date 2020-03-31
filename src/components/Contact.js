0f51f72d77825e6e3fc0e56edd1b2a3229c0f7f1
llaulier <lioraloche@yahoo.fr>
2020-03-31 12:39:01 +0200

    all problems are fixed, next step = end of the quest

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,13 +7,14 @@ function Contact(){
     return(
         <div className="Contact">
             <img className ="avatar" src ="https://randomuser.me/api/portraits/women/44.jpg" title ="avatar"></img>
-            <h4 className =" name">Rose Sutton
-            </h4>
-            <div className = "status">
-                <div className ="status-online">
+                <div><h4 className =" name">Rose Sutton
+                </h4>
+                    <div className = "status">
+                    <div className ="status-online">
+                    </div>
+                    <p className="status-text">{ online ? 'online': 'offline'}
+                    </p>
                 </div>
-                <p className="status-text">{ online ? 'online': 'offline'}
-                </p>
             </div>
         </div>
     );
