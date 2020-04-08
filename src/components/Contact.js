85f9a6e61725e1974d900ca4dacdf1e853289bc9
llaulier <lioraloche@yahoo.fr>
2020-04-08 11:48:58 +0200

    quete Le State 5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,49 @@
-import React from 'react';
-import './Contact.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./Contact.css";
+import PropTypes from "prop-types";
 
-
-function Contact(props) {
-    return(
-        <div className="Contact">
-            <img className ="avatar" src = {props.image} title ="avatar" alt={props.name}/>
-                <div><h4 className =" name">{props.name}
-                </h4>
-                    <div className = "status">
-                    <div className ={props.online ? "status-online" : "status-offline"}>
-                    </div>
-                    <p className="status-text">{props.online ? 'online': 'offline'}
-                    </p>
-                </div>
-            </div>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: this.props.online,
+    };
+  }
+  render() {
+    return (
+      <div className="Contact">
+        <img
+          className="avatar"
+          src={this.props.image}
+          title="avatar"
+          alt={this.props.name}
+        />
+        <div>
+          <h4 className=" name">{this.props.name}</h4>
+          <div
+            className="status"
+            onClick={(event) => {
+              const newOnline = !this.state.online;
+              this.setState({ online: newOnline });
+            }}
+          >
+            <div
+              className={this.state.online ? "status-online" : "status-offline"}
+            ></div>
+            <p className="status-text">
+              {this.state.online ? "online" : "offline"}
+            </p>
+          </div>
         </div>
+      </div>
     );
+  }
 }
 
 Contact.propTypes = {
-    online: PropTypes.bool.isRequired,
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-  };
+  online: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
 
 export default Contact;
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,38 +3,38 @@ import React from "react";
 import Contact from "./Contact.js";
 
 const users = [
-    {
-      name: 'Robert Reyes',
-      avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: false
-    },
-    {
-      name: 'Nellie Caldwell',
-      avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true
-    },
-    {
-      name: 'Vernon Mason',
-      avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: true
-    },
-    {
-      name: 'Erica Hunt',
-      avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
-    },
-    {
-      name: 'Juanita Phillips',
-      avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
-    }
-  ];
-  const ContactList = () => (
-    <div>
-      {users.map(item => (
-    <Contact name={item.name} image={item.avatar} online={item.online} />
-  ))}
-    </div>
-  );
+  {
+    name: "Robert Reyes",
+    avatar: "https://randomuser.me/api/portraits/men/18.jpg",
+    online: false,
+  },
+  {
+    name: "Caroline Sutton",
+    avatar: "https://randomuser.me/api/portraits/women/35.jpg",
+    online: true,
+  },
+  {
+    name: "Vernon Mason",
+    avatar: "https://randomuser.me/api/portraits/men/84.jpg",
+    online: true,
+  },
+  {
+    name: "Erica Hunt",
+    avatar: "https://randomuser.me/api/portraits/women/87.jpg",
+    online: false,
+  },
+  {
+    name: "Juanita Phillips",
+    avatar: "https://randomuser.me/api/portraits/women/55.jpg",
+    online: true,
+  },
+];
+const ContactList = () => (
+  <div>
+    {users.map((item) => (
+      <Contact name={item.name} image={item.avatar} online={item.online} />
+    ))}
+  </div>
+);
 
-  export default ContactList;
+export default ContactList;
